37c372fb7ebb1945e90105efb3db8605323dc689
Irving Rodríguez Romero <irvingrodriguezromero@gmail.com>
2021-11-30 11:27:16 -0600

    Modificación del archivo de conexión a la base de datos

--- php/conn.php
+++ php/conn.php
@@ -1,16 +1,16 @@
 <?php
 
-// $localhost='localhost';
-// $user='u439202014_sgcutd';
-// $ps='SgcUtd123_';
-// $db='u439202014_sgcutd';
-// $conexion = mysqli_connect($localhost, $user, $ps,$db);
-
 $localhost='localhost';
-$user='root';
-$ps='';
-$db='sgc_utd';
+$user='u439202014_sgcutd';
+$ps='SgcUtd123_';
+$db='u439202014_sgcutd';
 $conexion = mysqli_connect($localhost, $user, $ps,$db);
+
+// $localhost='localhost';
+// $user='root';
+// $ps='';
+// $db='sgc_utd';
+// $conexion = mysqli_connect($localhost, $user, $ps,$db);
 
 if ($conexion->connect_errno) {
   echo "Ocurrió un error al intentar conectar la app con la base de datos: (" . $conexion->connect_errno . ") " . $conexion->connect_error;
